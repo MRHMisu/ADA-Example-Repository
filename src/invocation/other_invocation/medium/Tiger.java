a94a39b8d68a12ded62f09099a0b12fc79209662
MRHMisu <md.rakib.hossain.19@ucl.ac.uk>
2022-02-10 11:21:39 -0800

    initialize

---
+++ src/invocation/other_invocation/medium/Tiger.java
@@ -0,0 +1,11 @@
+package invocation.other_invocation.medium;
+
+public class Tiger {
+
+    public static void main(String[] args) {
+
+    }
+
+    public Tiger(Teeth teeth, int length, int width, String type, String mcType, String smcType) {}
+
+}
---
+++ src/invocation/package_invocation/Farmer.java
@@ -0,0 +1,26 @@
+package invocation.package_invocation;
+
+//import com.google.common.base.Strings;
+
+import declaration.package_declaration.Snake;
+import invocation.attribute_invocation.*;
+import invocation.other_invocation.medium.Teeth;
+
+import java.util.Set;
+
+public class Farmer extends Teeth {
+
+    Horse horse;
+
+    HorseColor horseColor;
+
+    Donkey donkey;
+
+    Snake snake;
+
+    Set<String> set;
+
+    public void farm() {
+        //Strings.repeat("HEY", 5);
+    }
+}
